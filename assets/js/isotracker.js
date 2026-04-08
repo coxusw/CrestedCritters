6331e71426a7883a3fdc37dfd07527f00c1c107a
coxusw <50075513+coxusw@users.noreply.github.com>
2026-04-08 12:24:23 -0500

    Update default logo path in isotracker.js

--- assets/js/isotracker.js
+++ assets/js/isotracker.js
@@ -1,5 +1,5 @@
 const STORAGE = "isoTrackerData_v4";
-const DEFAULT_LOGO = "/assets/images/crested-critters-logo.png";
+const DEFAULT_LOGO = "/assets/images/logo.png";
 
 let activeColonyCategory = "All";
 let activeColonyType = "All";
